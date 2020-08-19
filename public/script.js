a796175f0bdacb10808478e6e3f9cee119b2ade2
Hanna Möllergren <hannamollergren@gmail.com>
2020-08-19 13:23:41 +0200

    new rout - /boats, html och css

--- public/script.js
+++ public/script.js
@@ -1,10 +1,27 @@
 window.addEventListener('load', () => {
 	let buttonGetBoats = document.querySelector('#btn-allboats');
+	let productList = document.querySelector('#product-list')
 
-	buttonGetBoats.addEventListener('click', () => {
+	buttonGetBoats.addEventListener('click', async () => {
+
 		console.log('Click works, buttonGetBoats')
+
+
+		const response = await fetch('/boats', { method: 'GET' });
+        const object = await response.json();  // motsvarar JSON.parse
+
+		console.log('Fetch returned:', object);
+		// 1 byt url till /boats/
+		// 2 hämta data
+
+       /*  object.forEach(boat => {
+			let ul = document.createElement('ul') 
+			il.setAttribute("id", "productList");
+
+			  
+            let li = document.createElement('li');
+			li.innerHTML = `Model: ${boat.model}<br>Year: ${boat.year}<br>`
+            productList.appendChild(li)
+        }) */
 	})
-
-
-
 })
--- server.js
+++ server.js
@@ -6,10 +6,28 @@ const port = 1234; // Port number
 // MIDDLEWARE 
 app.use(express.static(__dirname + '/public')) 
 
+// DATA
+let boat = {
+	id: '',
+	model: 'Marin',
+	year: 2010,
+	price: 134000.00,
+	sail: 'Yes',
+	motor: 'No',
+	image: ''
+}
+
 // REQUEST
 app.get('/', (req, res) => {
 	console.log('GET / index.html')
 	res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/boats/', (req, res) => {
+	// req.params
+	console.log('GET / boats.html')
+	/* res.send(boat) */
+	res.sendFile(__dirname + '/public/boats.html') //denna ska byta till denna filen och skriva ut datan
 })
 
 app.listen(port, () => {
