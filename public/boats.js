3e3664e567d894c27facbc6b9ce862d03ec1cdda
Hanna Möllergren <hannamollergren@gmail.com>
2020-08-24 16:43:27 +0200

    add boat works, bug on radio button

--- public/boats.js
+++ public/boats.js
@@ -1,5 +1,41 @@
 window.addEventListener('load', async () => {
 	let container = document.querySelector('.container')
+	let input = '';
+	let inputModel = document.querySelector('#inputModel');
+	let inputYear = document.querySelector('#inputYear');
+	let inputPrice = document.querySelector('#inputPrice');
+	let inputIsSail = document.querySelector('#inputIsSail');
+	let inputHasMotor = document.querySelector('#inputHasMotor');
+	let btnAddBoat = document.querySelector('.btn-addboat');
+	
+
+	btnAddBoat.addEventListener('click', async () => {
+		console.log('btnAddboat click')
+		input = {
+			id: '',
+			model: inputModel.value,
+			year: inputYear.value,
+			price: inputPrice.value,
+			is_sail: inputIsSail.value,
+			has_motor: inputHasMotor.value,
+			image: ''
+		}
+		console.log('input båt', input)
+
+		
+
+		const response = await fetch('/boat', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(input)
+        });
+        const text = await response.text();
+        console.log(text);
+	})
+
+
 
 	const response = await fetch('/boats', { method: 'GET' });
 	const object = await response.json();  
--- server.js
+++ server.js
@@ -1,57 +1,102 @@
 const express = require('express')
+const bodyParser = require('body-parser');	
 const app = express()
 
 const port = 1234; // Port number
 
 // MIDDLEWARE 
+app.use( (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+} )
+
 app.use(express.static(__dirname + '/public')) 
 
+app.use( bodyParser.urlencoded({ extended: true }) )
+app.use( bodyParser.json() )
+
 // DATA
-let boat = [
+let boats = [
 	{
-	id: '',
-	model: 'Marin',
-	year: 2010,
-	price: 134000.77,
-	is_sail: 'Yes',
-	has_motor: 'No',
-	image: ''
+		id: '',
+		model: 'Marin',
+		year: 2010,
+		price: 134000.77,
+		is_sail: 'Yes',
+		has_motor: 'No',
+		image: ''
 	},
 	{
-	id: '',
-	model: 'Aquador',
-	year: 2014,
-	price: 18888000.88,
-	is_sail: 'Yes',
-	has_motor: 'No',
-	image: ''
+		id: '',
+		model: 'Aquador',
+		year: 2014,
+		price: 18888000.88,
+		is_sail: 'Yes',
+		has_motor: 'No',
+		image: ''
 	},
 	{
-	id: '',
-	model: 'Nimbus',
-	year: 2099,
-	price: 14555500.99,
-	is_sail: 'No',
-	has_motor: 'Yes',
-	image: ''
+		id: '',
+		model: 'Nimbus',
+		year: 2099,
+		price: 14555500.99,
+		is_sail: 'No',
+		has_motor: 'Yes',
+		image: ''
 	},
 	{
-	id: '',
-	model: 'Black Pearl',
-	year: 1890,
-	price: 18888000.89,
-	is_sail: 'Yes',
-	has_motor: 'No',
-	image: ''
+		id: '',
+		model: 'Black Pearl',
+		year: 1890,
+		price: 18888000.89,
+		is_sail: 'Yes',
+		has_motor: 'No',
+		image: ''
 	},
 	{
-	id: '',
-	model: 'Buster',
-	year: 1990,
-	price: 155550.99,
-	is_sail: 'Yes',
-	has_motor: 'No',
-	image: ''
+		id: '',
+		model: 'Buster',
+		year: 1990,
+		price: 155550.99,
+		is_sail: 'Yes',
+		has_motor: 'No',
+		image: ''
+	},
+	{
+		id: '',
+		model: 'Buster',
+		year: 1990,
+		price: 155550.99,
+		is_sail: 'Yes',
+		has_motor: 'No',
+		image: ''
+	},
+	{
+		id: '',
+		model: 'Sea Ray',
+		year: 2020,
+		price: 10000000.99,
+		is_sail: 'No',
+		has_motor: 'Yes',
+		image: ''
+	},
+	{
+		id: '',
+		model: 'Princess',
+		year: 2022,
+		price: 178889999.99,
+		is_sail: 'Yes',
+		has_motor: 'No',
+		image: ''
+	},
+	{
+		id: '',
+		model: 'X900S',
+		year: 1986,
+		price: 30000088.99,
+		is_sail: 'No',
+		has_motor: 'Yes',
+		image: ''
 	}
 ];
 
@@ -61,11 +106,26 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html')
 })
 
+// GET /boats/
 app.get('/boats/', (req, res) => {
 	console.log('GET / boats')
-	res.send(boat)
-	/* res.sendFile(__dirname + '/public/boats.html') //denna ska byta till denna filen och skriva ut datan */
+	res.send(boats)
 })
+
+// GET /boat?id=x
+app.get('/boat', (req, res) => {
+    let id = Number(req.query.id)
+    res.send( boats[id] )
+}) 
+
+// POST
+app.post('/boat', (req, res) => {
+	let newBoat = {model: req.body.model, year: req.body.year, price: req.body.price, is_sail: req.body.is_sail, has_motor: req.body.has_motor}
+	console.log('newBoat', newBoat)
+	boats.push(newBoat);
+	res.send('New boat added.');
+})
+
 
 app.listen(port, () => {
 	console.log('Web server listening on port ' + port)
